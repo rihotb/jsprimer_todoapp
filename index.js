851b067f35b9405e48b7078d90a4eaaea56e74cf
riho <rihoriho1031@gmail.com>
2020-07-15 11:20:45 +0900

    feat: add todoapp

---
+++ index.js
@@ -0,0 +1,10 @@
+import { App } from "./src/App.js";
+const app = new App();
+
+window.addEventListener("load", () => {
+  app.mount();
+})
+
+window.addEventListener("unload", () => {
+  app.unmount();
+})
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { render } from "./view/html-util.js"
+import { TodoListModel } from "./model/TodoListModel.js";
+import { TodoItemModel } from "./model/TodoItemModel.js"
+import { TodoListView } from "./view/TodoListView.js";
+
+export class App {
+
+  constructor() {
+    this.todoListView = new TodoListView();
+    this.todoListModel = new TodoListModel([]);
+  }
+
+  handleAdd(title) {
+    this.todoListModel.addTodo(new TodoItemModel({ title, completed: false }))
+  }
+
+  handleUpdate({ id, completed }) {
+    this.todoListModel.updateTodo({ id, completed })
+  }
+
+  handleDelete({ id }) {
+    this.todoListModel.deleteTodo({ id })
+  }
+
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector('#js-form-input');
+    const containerElement = document.querySelector('#js-todo-list');
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+
+    this.todoListModel.onChange(() => {
+      const todoItems = this.todoListModel.getTodoItems();
+      const todoListElement = this.todoListView.createElement(todoItems, {
+        onUpdateTodo: ({ id, completed }) => {
+          this.handleUpdate({ id, completed });
+        },
+        onDeleteTodo: ({ id }) => {
+          this.handleDelete({ id });
+        }
+      });
+      render(todoListElement, containerElement);
+      todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
+    });
+
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      if (!inputElement.value) {
+        return
+      }
+      this.handleAdd(inputElement.value)
+      inputElement.value = ""
+    })
+  }
+
+  unmount() {
+    const formElement = document.querySelector("#js-form");
+    formElement.removeEventListener("submit", event)
+  }
+
+}
